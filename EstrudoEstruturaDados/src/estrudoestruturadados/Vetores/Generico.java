5293d6b194063646ce225fecf32d27ce7b4060bd
Francisco Silva <assis.francis123@hotmail.com>
2024-10-20 10:18:18 -0300

    Adicionando arquivos a branch tvetor

---
+++ EstrudoEstruturaDados/src/estrudoestruturadados/Vetores/Generico.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estrudoestruturadados.Vetores;
+
+/**
+ *
+ * @author kim
+ * @param <T>
+ */
+public class Generico <T>{
+    
+    private T[ ] obj;
+    private int tamanho;
+    
+    public Generico(int capacidade){
+        
+        this.obj = (T[]) new Object[capacidade];
+        this.tamanho =0;
+    }
+    public void aumentarCapacidade(){
+        
+    }
+    public void addIni(T obj){
+        
+    }
+    public boolean addPos(int pos, T obj){
+        
+    }
+    public String toString(){
+        
+    }
+    public Object  encontrarObj(int pos, T obj){
+        
+    } 
+    public void removeObjQualquerPosicao(int pos){
+        
+    }
+}
